c36febc221543ccaaf10f4a599b3e04d577d332f
Magic_KID <hanjie20010619@gmail.com>
2024-01-13 23:21:31 +0800

    The rest of Basics

---
+++ Verilog_Language/Basics/Declaring_wires.v
@@ -0,0 +1,15 @@
+module top_module(
+	input a,
+	input b,
+	input c,
+	input d,
+	output out,
+	output out_n
+);
+	wire t1,t2;
+	assign t1 = a && b;
+	assign t2 = c && d;
+	assign out = t1 || t2;
+	assign out_n = 	~(t1 || t2);
+
+endmodule
